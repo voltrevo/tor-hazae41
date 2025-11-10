a03c62eee59766eeadd0e6aab97745c99f87b83e
Andrew Morris <voltrevo@gmail.com>
2025-11-10 13:18:24 -0300

    update tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/TorClient/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
   sourcemap: true,
@@ -11,18 +11,5 @@ export default defineConfig({
   outDir: 'dist',
   target: 'es2022',
   treeshake: false,
-  external: [
-    '@brumewallet/wallet.wasm',
-    '@hazae41/binary',
-    '@hazae41/cadenas',
-    '@hazae41/cascade',
-    '@hazae41/chacha20poly1305',
-    '@hazae41/echalote',
-    '@hazae41/ed25519',
-    '@hazae41/fleche',
-    '@hazae41/keccak256',
-    '@hazae41/ripemd160',
-    '@hazae41/sha1',
-    '@hazae41/x25519',
-  ],
+  external: ['*'],
 });
