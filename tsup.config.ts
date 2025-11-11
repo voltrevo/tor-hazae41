6672b8fd21c90dbd8f61d59616374a53da4da856
Andrew Morris <voltrevo@gmail.com>
2025-11-11 10:16:09 -0300

    add curlTor to bin

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/TorClient/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  minify: false,
-  splitting: false,
-  outDir: 'dist',
-  target: 'es2022',
-  treeshake: false,
-  external: ['*'],
-});
+export default defineConfig([
+  {
+    entry: ['src/TorClient/index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    minify: false,
+    splitting: false,
+    outDir: 'dist',
+    target: 'es2022',
+    treeshake: false,
+    external: ['*'],
+  },
+  {
+    entry: ['curlTor.ts'],
+    format: ['cjs'],
+    dts: false,
+    sourcemap: true,
+    minify: false,
+    outDir: 'dist',
+    target: 'es2022',
+    treeshake: false,
+    external: ['*'],
+    shims: true,
+    noExternal: ['./src/TorClient'],
+  },
+]);
