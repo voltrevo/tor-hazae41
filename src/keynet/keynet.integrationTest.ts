defe4245b03500430a7ee5fc74bfaaad369c3302
Andrew Morris <voltrevo@gmail.com>
2025-12-16 21:46:40 +0000

    fix keynet integration test

--- src/keynet/keynet.integrationTest.ts
+++ src/keynet/keynet.integrationTest.ts
@@ -1,15 +1,16 @@
 import { test } from '@hazae41/phobos';
-import { TorClient } from '../TorClient/versions/standard';
+import { Log, TorClient } from '../TorClient/versions/standard';
 
 test('Keynet connection through TorClient (transparent)', async () => {
   const keynetAddr =
     'http://jyn6dehf3ttu4lblc7tr3i23xqsz76dn2du6keptg5kyo3r6mur36vad.keynet/';
 
-  // TorClient.fetch transparently handles .keynet addresses
-  const response = await TorClient.fetch(
-    'wss://snowflake.pse.dev/',
-    keynetAddr
-  );
+  using tor = new TorClient({
+    snowflakeUrl: 'wss://snowflake.pse.dev/',
+    log: new Log(),
+  });
+
+  const response = await tor.fetch(keynetAddr);
 
   const txt = await response.text();
   console.log('Response received:', txt.substring(0, 100));
