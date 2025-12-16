957996c52fbbd9569ca33aab042654955d0fc056
Andrew Morris <voltrevo@gmail.com>
2025-12-16 22:07:00 +0000

    fix test

--- src/storage/getStorageName.test.ts
+++ src/storage/getStorageName.test.ts
@@ -3,7 +3,7 @@ import { assert } from '../utils/assert';
 import { PACKAGE_VERSION } from './getStorageName';
 import packageJson from '../../package.json' assert { type: 'json' };
 
-test('versionedStorageNamespace: constant matches package.json', async () => {
+test('getStorageName: constant matches package.json', async () => {
   assert(
     PACKAGE_VERSION === packageJson.version,
     `PACKAGE_VERSION (${PACKAGE_VERSION}) must match package.json version (${packageJson.version})`
