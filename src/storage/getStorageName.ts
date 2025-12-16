f56afbc4aa8916e33e924a9e4c8d2a22e5f5db2b
Andrew Morris <voltrevo@gmail.com>
2025-12-16 05:54:57 +0000

    change version to 0.1.0 (not releasing here)

--- src/storage/getStorageName.ts
+++ src/storage/getStorageName.ts
@@ -1,5 +1,5 @@
 // Package version - must match version in package.json
-export const PACKAGE_VERSION = '0.2.6';
+export const PACKAGE_VERSION = '0.1.0';
 
 export function getStorageName(): string {
   return `tor-js-${PACKAGE_VERSION}-cache`;
