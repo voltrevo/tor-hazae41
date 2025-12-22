79ed3381086ff7c0e8afb82d4b4e98853a7389e3
Andrew Morris <voltrevo@gmail.com>
2025-12-22 02:19:23 +0000

    remove semaphore

--- src/hazae41/mutex/index.manualTest.ts
+++ src/hazae41/mutex/index.manualTest.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { Mutex, Semaphore } from './index';
+import { Mutex } from './index';
 
 test('run', async () => {
   const promises = new Array<Promise<void>>();
@@ -115,53 +115,4 @@ test('acquire', async () => {
       ])
     );
   });
-});
-
-test('semaphore', async () => {
-  const promises = new Array<Promise<void>>();
-
-  const semaphore = new Semaphore(undefined, 3);
-
-  const tick = async () => {
-    for (let i = 0; i < 100; i++) {
-      console.log('tick');
-      await new Promise(ok => setTimeout(ok, 100));
-    }
-  };
-
-  const lock = (i: number) =>
-    semaphore.runOrWait(async () => {
-      console.log('start', i);
-      await new Promise(ok => setTimeout(ok, 1000));
-      console.log('end', i);
-    });
-
-  const wait = async () => {
-    console.log('wait');
-    await semaphore.runOrWait(() => {});
-    console.log('done');
-  };
-
-  const acquire = async () => {
-    using _ = await semaphore.lockOrWait();
-    console.log('acquire');
-    await new Promise(ok => setTimeout(ok, 1000));
-    console.log('release');
-  };
-
-  promises.push(tick());
-  promises.push(lock(1));
-  promises.push(lock(2));
-  promises.push(lock(3));
-  promises.push(acquire());
-  promises.push(lock(4));
-  promises.push(lock(5));
-  promises.push(wait());
-  promises.push(lock(6));
-  promises.push(lock(7));
-  promises.push(lock(8));
-  promises.push(lock(9));
-  promises.push(lock(10));
-
-  await Promise.all(promises);
 });
