2f7bf884f48a9558a61a6963ed929070d5e2202c
Andrew Morris <voltrevo@gmail.com>
2025-12-22 03:02:54 +0000

    0.2.3

--- src/storage/getStorageName.ts
+++ src/storage/getStorageName.ts
@@ -1,5 +1,5 @@
 // Package version - must match version in package.json
-export const PACKAGE_VERSION = '0.2.2';
+export const PACKAGE_VERSION = '0.2.3';
 
 export function getStorageName(): string {
   return `tor-js-${PACKAGE_VERSION}-cache`;
